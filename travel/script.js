ba19cbd5ce00f28dc7a044ca4b58ca871cff8824
int2itive <70378581+int2itive@users.noreply.github.com>
2024-08-01 10:31:43 +0100

    Update script.js

--- travel/script.js
+++ travel/script.js
@@ -1,6 +1,16 @@
 const menuBtn = document.querySelector('.menu-btn');
 const banner = document.querySelector('.header');
 const menuSlider = document.querySelector('.mbl--list-items');
+// const para = document.querySelector('.header--content-grid p');
+const main = document.querySelector('.headerDown');
+
+let hiddenState = "-hidden", nav_dark = "intuvenus--header";
+
+let threshold = 10,
+    uBound = 7, 
+    position = 0, 
+    lastScroll = 0, 
+    n_event = 0;
 
 
 let styles = getComputedStyle(banner);
@@ -86,6 +96,24 @@ menuBtn.addEventListener('click', () => {
     menuSlider.classList.remove('showing');
     menuOpen = false;
   }
+});
+
+window.addEventListener("scroll", function () {
+  var position = window.scrollY || document.documentElement.scrollTop;
+  
+  if (position > threshold && position > lastScroll) {
+    main.classList.add(hiddenState);
+  } else {
+    main.classList.remove(hiddenState);
+    if (window.scrollY > 40) {
+      main.classList.add(nav_dark);
+    } else {
+      main.classList.remove(nav_dark);
+    }
+  }
+
+  lastScroll = position <= 0 ? 0 : position;
+
 });
 
 // doProgressBar();
