4989cc04477b88860cd4319ffaac9efc9de29ba5
int2itive <70378581+int2itive@users.noreply.github.com>
2024-08-06 14:55:16 +0100

    Update script.js

--- about/script.js
+++ about/script.js
@@ -27,6 +27,16 @@ menuBtn.addEventListener('click', () => {
   }
 });
 
+// function doProgressBar() {
+const progress = document.querySelector('.progress--bar');
+document.addEventListener('scroll', () => {
+  const scroll = window.scrollY;
+  const height = document.documentElement.scrollHeight - window.innerHeight;
+  const progressWidth = Math.floor(100 * scroll / height);
+  progress.style.width = `${progressWidth}%`;
+}, { passive: true });
+// }
+
 window.addEventListener("scroll", function () {
   var position = window.scrollY || document.documentElement.scrollTop;
   
